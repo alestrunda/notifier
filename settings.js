3413ee7d721763e17b63cfd301a5c32ce51dfc22
Aleš Trunda <alestrunda@gmail.com>
2021-11-12 20:45:51 +0100

    add audio files

--- settings.js
+++ settings.js
@@ -6,7 +6,10 @@ export const MESSAGE = {
 
 export const AUDIO_FILES = {
   "goes-without-saying-608.mp3": "goes without saying",
+  "juntos-607.mp3": "juntos",
   "open-your-eyes-and-see-323.mp3": "open your eyes and see",
+  "quite-impressed-565.mp3": "quite impressed",
+  "slow-spring-board-longer-tail-571.mp3": "slow spring board longer tail",
 };
 
 export const STORAGE_KEYS = {
